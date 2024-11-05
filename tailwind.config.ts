99c581e8e3a9bdad489622f325c72cbec14191a2
Tristin Albers <1043042+tristin-albers@users.noreply.github.com>
2024-11-04 20:29:42 -0800

    feat: added DaisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,5 +11,17 @@ export default {
 		extend: {}
 	},
 
-	plugins: [typography, forms, containerQueries, aspectRatio]
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	plugins: [typography, forms, containerQueries, aspectRatio, require('daisyui')],
+
+	daisyui: {
+		themes: ["nord","dim"],
+		darkTheme: "dim", 
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+		themeRoot: ":root",
+	},
 } satisfies Config;
